c23c401745d58259092bf52b59c5f392ef9fb2d0
Alert123456 <43949037+Alert123456@users.noreply.github.com>
2022-06-03 00:17:52 +0800

    Python第四章学习
    Python学习第四章开始

---
+++ PythonCode/Python36BaseLearning/Python编程第四章.py
@@ -0,0 +1,20 @@
+magicians = ['alice','david','carolina']
+for magician in magicians:
+    print(magician.title() + ", that was a great trick!")
+    print("I can't wait to see your next trick, " + magician.title() + ".\n")
+print("Thank you, everyone. That was a great magic show!")
+
+#习题4-1
+print("习题4-1：\n")
+Pizza_Names = ["fruit pizza",'liulian pizza','beef pizza']
+for Pizza_Name in Pizza_Names:
+    print("I like "+Pizza_Name)
+print("I really love pizza")
+print("习题4-2：\n")
+animals = ['dog','cat','mouse']
+for animal in animals:
+    print("A "+animal+" would make a great pet!")
+print("Any of these animals would make a great pet!")
+
+for value in range(1,5):
+    print(value)
