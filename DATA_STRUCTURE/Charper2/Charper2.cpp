4269b4f1ebbcb1e3c153c30e0e9f39fdd5681a1b
albert <albert@example.com>
2022-09-18 18:12:48 +0800

    数据结构--算法通识
    学完第二章

--- DATA_STRUCTURE/Charper2/Charper2.cpp
+++ DATA_STRUCTURE/Charper2/Charper2.cpp
@@ -16,18 +16,47 @@ using namespace std;
 //     return 0;
 // }
 // 求和 1-10000 用神奇算法
+// int main()
+// {
+//     int sum = 0, n = 100, x = 0;            /* 执行1次 */
+//     for(int i = 1; i <= n; i++)             /* 执行n+1次 */
+//     {
+//         for (size_t j = 0; j < n; j++)  
+//         {
+//             x++;
+//             sum = sum + x;                  /* 执行n*n次 */
+//         } 
+//     }
+//     cout << sum;                            /* 执行1次 */
+//     return 0;
+// }
+// // 求和 1-100 用高斯的算法
+// int main()
+// {
+//     int sum = 0, n = 100;           /* 执行1次 */
+//     sum = (1 + n) * n / 2;          /* 执行1次 */
+//     cout << sum;                    /* 执行1次 */
+//     return 0;
+// }
+// // 复杂度O[n]
+// int main()
+// {
+//     int i, n = 10, sum = 0;
+//     for(i = 0; i < n; i++)
+//     {
+//         sum += i;
+//     }
+//     cout << sum << endl;
+//     return 0;
+// }
+// 复杂度O[logn]
 int main()
 {
-    int sum = 0, n = 100, x = 0;            /* 执行1次 */
-    for(int i = 1; i <= n; i++)             /* 执行n+1次 */
+    int count = 1, n = 100;
+    while (count < n)
     {
-        for (size_t j = 0; j < n; j++)  
-        {
-            x++;
-            sum = sum + x;                  /* 执行n*n次 */
-        } 
+        count = count * 2;
     }
-    cout << sum;                            /* 执行1次 */
+    
     return 0;
 }
-// 求和 1-100 用高斯的算法
